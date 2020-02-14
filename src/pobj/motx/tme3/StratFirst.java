735e7017bf69d87784e515acab4ac97b16044608
victor.oltean <victor.oltean@etu.upmc.fr>
2020-02-14 11:31:32 +0100

    stratfirst

--- src/pobj/motx/tme3/StratFirst.java
+++ src/pobj/motx/tme3/StratFirst.java
@@ -2,4 +2,11 @@ package pobj.motx.tme3;
 
 public class StratFirst implements IChoixVar {
 
+  public StratFirst() {
+
+  }
+
+  IVariable chooseVar(ICSP problem) {
+    return problem.getVars().get(0);
+  }
 }
